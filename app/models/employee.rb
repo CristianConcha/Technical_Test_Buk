af12d5af70897d0af69beac21643000a57e9b51c
CristianConcha <ciconcha@miuandes.cl>
2025-01-27 11:07:19 -0300

    feat: create progressive vacations method and 3 test for the method, and reuse new private function total_months_same_company

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -10,10 +10,30 @@ class Employee
 
   # Methods
   def normal_vacations(query_date)
-
-    total_months = (query_date.year * 12 + query_date.month) - (@start_date.year * 12 + @start_date.month)
+    total_months = total_months_same_company(query_date)
 
     total_vacations = (total_months >= 12 && @total_worked_years >= 1) ? 15 : 0
+  end
+
+  def progressive_vacations(query_date)
+    if @last_three_years_same_company && @total_worked_years >= 10
+      total_months_same_company = total_months_same_company(query_date)
+      years_same_company = (total_months_same_company / 12).to_i
+      extra_years = @total_worked_years - 10
+      relevant_years_for_progressive = [years_same_company, extra_years].min
+      additional_days = (relevant_years_for_progressive / 3).floor
+      total_vacations = normal_vacations(query_date) + additional_days
+    else
+      total_vacations = 0
+    end
+
+    total_vacations
+  end
+
+  private
+
+  def total_months_same_company(query_date)
+    (query_date.year * 12 + query_date.month) - (@start_date.year * 12 + @start_date.month)
   end
 
 end
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -13,4 +13,39 @@ class EmployeeTest < ActiveSupport::TestCase
 
     assert_equal 15, employee.normal_vacations(query_date)
   end
+
+  # Test for the method progressive vacations
+  test "employee with 13 years of work and last 3 years at the same company should have 16 days of vacation" do
+    employee = Employee.new(
+      start_date: Date.new(2013, 1, 1),
+      total_worked_years: 13,
+      last_three_years_same_company: true
+    )
+    query_date = Date.new(2023, 1, 1)
+
+    assert_equal 16, employee.progressive_vacations(query_date)
+  end
+
+  test "employee with 16 years of work and last 6 years at the same company should have 17 days of vacation" do
+    employee = Employee.new(
+      start_date: Date.new(2007, 1, 1),
+      total_worked_years: 16,
+      last_three_years_same_company: true
+    )
+    query_date = Date.new(2023, 1, 1)
+
+    assert_equal 17, employee.progressive_vacations(query_date)
+  end
+
+  test "employee with 19 years of work and last 9 years at the same company should have 18 days of vacation" do
+    employee = Employee.new(
+      start_date: Date.new(2007, 1, 1),
+      total_worked_years: 19,
+      last_three_years_same_company: true
+    )
+    query_date = Date.new(2023, 1, 1)
+
+    assert_equal 18, employee.progressive_vacations(query_date)
+  end
+
 end
